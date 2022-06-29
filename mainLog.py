69211cf701655fd6b2aca948727bf69a40171655
danqunyu <danqunyu@163.com>
2022-06-30 01:13:09 +0800

    add more things into main log

--- mainLog.py
+++ mainLog.py
@@ -2,4 +2,7 @@
 create local branch, and add file;
 then check out to main branch, and add mai log before merge.
 
+just want to add more contentes in to main log before merging
+
+this is a test. test and test more
 '''
